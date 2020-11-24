acb4d53190c56560fe6bb20ae9c618032e94796f
Romullo <developermarsh@gmail.com>
2020-11-24 00:25:49 -0300

    feat(app): added router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core'
+
+import { AuthComponent } from './auth/auth.component'
+import { ConsoleComponent } from './console/console.component'
+
+import { RouterModule, Routes } from '@angular/router'
+
+const routes: Routes = [
+  { path: 'auth', component: AuthComponent },
+  { path: 'console', component: ConsoleComponent },
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
