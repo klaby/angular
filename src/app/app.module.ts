314521440c33010ae81849de6019484092cd6e07
Romullo <developermarsh@gmail.com>
2020-11-24 00:23:19 -0300

    feat(app): added module auth and console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
+import { AuthComponent } from './auth/auth.component'
+import { ConsoleComponent } from './console/console.component'
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent, AuthComponent, ConsoleComponent],
+  imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
