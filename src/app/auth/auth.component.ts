0bc1c7b719830d9cc7656b661100392d299b018c
Romullo <developermarsh@gmail.com>
2020-12-09 20:33:13 -0300

    chore(auth): simple authentication flow
    - Testing routing.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core'
-
+import { AuthService } from './auth.service'
+import { CookieService } from 'ngx-cookie-service'
+import { Observable, of } from 'rxjs'
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
+  providers: [CookieService],
 })
 export class AuthComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) {}
 
-  ngOnInit(): void {}
+  auth() {
+    this.authService.signin().subscribe(signed => {
+      if (signed) {
+        this.router.navigate(['/console'])
+      }
+    })
+  }
+
+  ngOnInit() {}
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core'
-
+import { CookieService } from 'ngx-cookie-service'
+import { Observable, of } from 'rxjs'
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private readonly cookieService: CookieService) {}
+
+  signin(): Observable<boolean> {
+    const token = Math.random().toString(36).substr(2, 9)
+
+    this.cookieService.set('_a', token)
+    return of(true)
+  }
+
+  isSigned(): Observable<boolean> {
+    return of(!!this.cookieService.get('_a'))
+  }
 }
